39b13fdc5ee3b60e12e5d31aa95bd89cb9360163
Piyush <piyushkatkar9421@gmail.com>
2026-01-14 20:19:40 +0530

    update dependencies

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -24,3 +24,4 @@ pub const MIN_TARGET: U256 = U256([
 ]);
 pub const DIFICULTY_UPDATE_INTERVAL: u64 = 50;
 pub const MAX_MEMPOOL_TRANSACTION_AGE: u64 = 600;
+pub const BLOCK_TRANSACTION_CAP: usize = 20;
